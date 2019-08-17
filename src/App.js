6efbde5429a8b33788d954173ede0e97c153b16d
Yukiko Whalen <whalenyukiko@gmail.com>
2019-08-18 00:49:13 +0900

    fixed index.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,254 @@
+// import React, { Component } from 'react';
+// import Particles from 'react-particles-js';
+// import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+// import Navigation from './components/Navigation/Navigation';
+// import Signin from './components/Signin/Signin';
+// import Register from './components/Register/Register';
+// import Logo from './components/Logo/Logo';
+// import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+// import Rank from './components/Rank/Rank';
+// import './App.css';
+
+// const particlesOptions = {
+//   particles: {
+//     number: {
+//       value: 30,
+//       density: {
+//         enable: true,
+//         value_area: 800
+//       }
+//     }
+//   }
+// }
+
+// const initialState = {
+//   input: '',
+//   imageUrl: '',
+//   box: {},
+//   route: 'signin',
+//   isSignedIn: false,
+//   user: {
+//     id: '',
+//     name: '',
+//     email: '',
+//     entries: 0,
+//     joined: ''
+//   }
+// }
+
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = initialState;
+//   }
+
+//   loadUser = (data) => {
+//     this.setState({user: {
+//       id: data.id,
+//       name: data.name,
+//       email: data.email,
+//       entries: data.entries,
+//       joined: data.joined
+//     }})
+//   }
+
+//   calculateFaceLocation = (data) => {
+//     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+//     const image = document.getElementById('inputimage');
+//     const width = Number(image.width);
+//     const height = Number(image.height);
+//     return {
+//       leftCol: clarifaiFace.left_col * width,
+//       topRow: clarifaiFace.top_row * height,
+//       rightCol: width - (clarifaiFace.right_col * width),
+//       bottomRow: height - (clarifaiFace.bottom_row * height)
+//     }
+//   }
+
+//   displayFaceBox = (box) => {
+//     this.setState({box: box});
+//   }
+
+//   onInputChange = (event) => {
+//     this.setState({input: event.target.value});
+//   }
+
+//   onButtonSubmit = () => {
+//     this.setState({imageUrl: this.state.input});
+//       fetch('https://git.heroku.com/guarded-taiga-93650.git/imageurl', {
+//         method: 'post',
+//         headers: {'Content-Type': 'application/json'},
+//         body: JSON.stringify({
+//           input: this.state.input
+//         })
+//       })
+//       .then(response => response.json())
+//       .then(response => {
+//         if (response) {
+//           fetch('https://git.heroku.com/guarded-taiga-93650.git/image', {
+//             method: 'put',
+//             headers: {'Content-Type': 'application/json'},
+//             body: JSON.stringify({
+//               id: this.state.user.id
+//             })
+//           })
+//             .then(response => response.json())
+//             .then(count => {
+//               this.setState(Object.assign(this.state.user, { entries: count}))
+//             })
+//             .catch(console.log)
+
+//         }
+//         this.displayFaceBox(this.calculateFaceLocation(response))
+//       })
+//       .catch(err => console.log(err));
+//   }
+
+//   onRouteChange = (route) => {
+//     if (route === 'signout') {
+//       this.setState(initialState)
+//     } else if (route === 'home') {
+//       this.setState({isSignedIn: true})
+//     }
+//     this.setState({route: route});
+//   }
+
+//   render() {
+//     const { isSignedIn, imageUrl, route, box } = this.state;
+//     return (
+//       <div className="App">
+//          <Particles className='particles'
+//           params={particlesOptions}
+//         />
+//         <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+//         { route === 'home'
+//           ? <div>
+//               <Logo />
+//               <Rank
+//                 name={this.state.user.name}
+//                 entries={this.state.user.entries}
+//               />
+//               <ImageLinkForm
+//                 onInputChange={this.onInputChange}
+//                 onButtonSubmit={this.onButtonSubmit}
+//               />
+//               <FaceRecognition box={box} imageUrl={imageUrl} />
+//             </div>
+//           : (
+//              route === 'signin'
+//              ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+//              : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+//             )
+//         }
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+
+// import React from 'react';
+
+// class Register extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       email: '',
+//       password: '',
+//       name: ''
+//     }
+//   }
+
+//   onNameChange = (event) => {
+//     this.setState({name: event.target.value})
+//   }
+
+//   onEmailChange = (event) => {
+//     this.setState({email: event.target.value})
+//   }
+
+//   onPasswordChange = (event) => {
+//     this.setState({password: event.target.value})
+//   }
+
+//   onSubmitSignIn = () => {
+//     fetch('https://git.heroku.com/guarded-taiga-93650.git/register', {
+//       method: 'post',
+//       headers: {'Content-Type': 'application/json'},
+//       body: JSON.stringify({
+//         email: this.state.email,
+//         password: this.state.password,
+//         name: this.state.name
+//       })
+//     })
+//       .then(response => response.json())
+//       .then(user => {
+//         if (user.id) {
+//           this.props.loadUser(user)
+//           this.props.onRouteChange('home');
+//         }
+//       })
+//   }
+
+//   render() {
+//     return (
+//       <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+//         <main className="pa4 black-80">
+//           <div className="measure">
+//             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+//               <legend className="f1 fw6 ph0 mh0">Register</legend>
+//               <div className="mt3">
+//                 <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
+//                 <input
+//                   className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+//                   type="text"
+//                   name="name"
+//                   id="name"
+//                   onChange={this.onNameChange}
+//                 />
+//               </div>
+//               <div className="mt3">
+//                 <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+//                 <input
+//                   className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+//                   type="email"
+//                   name="email-address"
+//                   id="email-address"
+//                   onChange={this.onEmailChange}
+//                 />
+//               </div>
+//               <div className="mv3">
+//                 <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+//                 <input
+//                   className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+//                   type="password"
+//                   name="password"
+//                   id="password"
+//                   onChange={this.onPasswordChange}
+//                 />
+//               </div>
+//             </fieldset>
+//             <div className="">
+//               <input
+//                 onClick={this.onSubmitSignIn}
+//                 className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+//                 type="submit"
+//                 value="Register"
+//               />
+//             </div>
+//           </div>
+//         </main>
+//       </article>
+//     );
+//   }
+// }
+
+// export default Register;
+
+
+
+
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
@@ -75,7 +326,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch('https://git.heroku.com/guarded-taiga-93650.git/imageurl', {
+      fetch('http://localhost:3000/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -85,7 +336,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('https://git.heroku.com/guarded-taiga-93650.git/image', {
+          fetch('http://localhost:3000/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
